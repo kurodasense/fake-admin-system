1a9e0bc64b81f42994ff404794aa9440530f3a9e
kurodasense <1205918114@qq.com>
2024-02-04 17:02:19 +0800

    hahaha

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,13 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier/skip-formatting"
+    "@vue/eslint-config-prettier/skip-formatting",
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
     ecmaVersion: "latest"
   },
-  "env": {
-    "node": true
+  env: {
+    node: true
   }
 };
